a259801af8d5622ebfa7ae1a7f833df29aa69981
Keith Woods <devshop7@gmail.com>
2015-06-20 14:32:58 +0100

    adding license headers to each file via grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
-/*
+/* notice_start
  * Copyright 2015 Keith Woods
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
-"use strict";
+ notice_end */
 
 module.exports = function(grunt) {
 
     var webpack = require('webpack');
 
     require('load-grunt-tasks')(grunt, {pattern: ['grunt-*']});
+
+    var licenseNotice = grunt.file.read('NOTICE');
 
     grunt.initConfig({
         jshint: {
@@ -131,17 +131,38 @@ module.exports = function(grunt) {
                 }]
             }
         },
+        usebanner: {
+            dist: {
+                options: {
+                    position: 'top',
+                    banner: licenseNotice
+                },
+                files: {
+                    src: [ 'dist/microdi.js', 'dist/microdi.min.js' ]
+                }
+            },
+            es6Src: {
+                options: {
+                    position: 'top',
+                    banner: licenseNotice
+                },
+                files: {
+                    src: [ 'index.js', 'Gruntfile.js', 'karma.conf.js', 'src/**/*.js', 'tests/**/*.js']
+                }
+            }
+        },
         watch: {
             options: {
                 atBegin: true
             },
             files: ['<%= jshint.src.files.src %>', '<%= jshint.tests.files.src %>'],
-            tasks: ['jshint', 'karma:unit:run', 'webpack', 'babel']
+            tasks: ['jshint', 'karma:unit:run', 'webpack', 'babel', 'usebanner:dist']
         }
     });
 
-    grunt.registerTask('build', ['jshint', 'webpack', 'babel']);
+    grunt.registerTask('build', ['jshint', 'webpack', 'babel', 'usebanner:dist']);
     grunt.registerTask('test', ['build', 'karma:release']);
     grunt.registerTask('dev', ['karma:unit:start', 'watch']);
+    grunt.registerTask('updateEs6License', ['usebanner:es6Src']);
     grunt.registerTask('default', ['test']);
 };

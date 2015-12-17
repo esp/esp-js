66c408f0bcabbfff695befd4f95c7e71fc9a0506
Ray Booysen <ray@sumatro.co.uk>
2015-12-17 22:07:57 +0000

    Proper copyright

--- tests/system/disposables/dictionaryDisposableTests.js
+++ tests/system/disposables/dictionaryDisposableTests.js
@@ -1,6 +1,6 @@
 // notice_start
 /*
- * Copyright 2015 Keith Woods
+ * Copyright 2015 Dev Shop Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

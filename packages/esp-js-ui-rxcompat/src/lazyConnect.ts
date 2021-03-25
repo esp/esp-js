039feb61f47a8ea6e36f57d78f27fbbee0d16e72
Keith Woods <devshop7@gmail.com>
2021-03-25 13:27:59 +0800

    Tweak to the lazyConnect compat function

--- packages/esp-js-ui-rxcompat/src/lazyConnect.ts
+++ packages/esp-js-ui-rxcompat/src/lazyConnect.ts
@@ -1,13 +1,13 @@
-import {Observable, Subscription} from 'rxjs';
+import {Observable, Subscription, ConnectableObservable} from 'rxjs';
 import {lazyConnect} from 'esp-js-ui';
 
 export function lazyConnectCompat<T>(onConnect: (subscription: Subscription) => void): Observable<T> {
     return lazyConnect<T>(onConnect)(this);
 }
 // Note, this should really be on type ConnectableObservable but I can't seem to get that working.
-(Observable as any).prototype.lazyConnect = lazyConnectCompat;
-declare module 'rxjs/internal/Observable' {
-    interface Observable<T> {
+(ConnectableObservable as any).prototype.lazyConnect = lazyConnectCompat;
+declare module 'rxjs/internal/observable/ConnectableObservable' {
+    interface ConnectableObservable<T> {
         lazyConnect: typeof lazyConnectCompat;
     }
 }

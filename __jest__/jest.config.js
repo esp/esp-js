712ee4699b66d556039796b5597d2953314dfb86
Keith Woods <devshop7@gmail.com>
2021-05-22 11:17:38 +0800

    Added new jest console logger so it doens't wrap every single log line with additional junk

--- __jest__/jest.config.js
+++ __jest__/jest.config.js
@@ -16,6 +16,7 @@ module.exports = {
         "\\.(css|less)$": "<rootDir>/../../__jest__/mocks/styleMock.js"
     },
     "setupFiles": [
+        "<rootDir>/../../__jest__/disableJestConsoleWrapper.js",
         "<rootDir>/../../__jest__/mocks/browserMocks.js"
     ],
     "modulePaths": [

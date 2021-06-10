d606e124d17a2320efffab33144ff4c6e5be7bcf
Keith Woods <devshop7@gmail.com>
2021-06-10 13:22:37 +0800

    Importing and reexporting esp-js-rx bits from within esp-js-ui to maintain backwards compat

--- packages/esp-js-ui/src/core/index.ts
+++ packages/esp-js-ui/src/core/index.ts
@@ -2,6 +2,7 @@
 export * from './decimal';
 export * from './decimalFormat';
 export * from './environment';
+export * from './observableExt';
 export * from './schedulerService';
 export * from './unit';
 export * from './utils';
---
+++ packages/esp-js-ui/src/core/observableExt.ts
@@ -0,0 +1,25 @@
+// The observable apis used to be in this package but were moved to esp-js-rx.
+// To maintain backwards compat we import and re export them.
+import {
+    doOnSubscribe,
+    lazyConnect,
+    ValueAndModel,
+    EspRouterObservable,
+    liftToEspObservable,
+    RetryPolicyLike,
+    RetryPolicy,
+    retryWithPolicy,
+    takeUntilInclusive,
+} from 'esp-js-rx';
+
+export {
+    doOnSubscribe,
+    lazyConnect,
+    ValueAndModel,
+    EspRouterObservable,
+    liftToEspObservable,
+    RetryPolicyLike,
+    RetryPolicy,
+    retryWithPolicy,
+    takeUntilInclusive,
+};

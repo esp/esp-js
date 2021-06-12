1e110b6bb5b5d50d6be969c5b849aa7a9e849d51
Keith Woods <devshop7@gmail.com>
2021-06-12 15:04:29 +0800

    Removing some beta operators from esp-js-rx, might at back at some point

--- packages/esp-js-rx/src/operators/index.ts
+++ packages/esp-js-rx/src/operators/index.ts
@@ -1,9 +1,7 @@
 // Auto-generated 
-export * from './bufferWithSnapshot';
 export * from './doOnSubscribe';
 export * from './espObservable';
 export * from './lazyConnect';
-export * from './restOnError';
 export * from './retryPolicy';
 export * from './retryWithPolicy';
 export * from './takeUntilInclusive';

e6b51cf0b97b40155a39ca510413b6beb76fa748
Keith Woods <devshop7@gmail.com>
2024-12-16 20:44:37 +0000

    Changing React peerDependencies for esp-js-react and esp-js-ui to allow it to work with v ^18.0.0 || ^19.0.0. Updated example app so top level view is a function component.

--- examples/esp-js-ui-module-based-app/src/index.tsx
+++ examples/esp-js-ui-module-based-app/src/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import {createRoot} from 'react-dom/client';
 import {AppShell} from './shell/appShell';
 import './css/espdemo.css';
+import {AppShellView} from './shell/views/shell/shellView';
 
 let appShell = new AppShell();
 appShell.start();
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(appShell.rootElement);
+root.render(<AppShellView router={appShell.router} workspaceModelId={appShell.workspaceModelId} />);
--- examples/esp-js-ui-module-based-app/tests/appTests.tsx
+++ examples/esp-js-ui-module-based-app/tests/appTests.tsx
@@ -5,13 +5,16 @@ import * as React from 'react';
 // provides a set of custom jest matchers that you can use to extend jest
 // i.e. `.toBeVisible`
 import '@testing-library/jest-dom';
+import {AppShellView} from '../src/shell/views/shell/shellView';
 
 describe('ShellBootstrapper Tests', () => {
     // This is a high level integration test to see if the example actually loads
     it('Catch all integration tests', () => {
-        let shellBootstrapper = new AppShell();
-        shellBootstrapper.start();
-        const renderResult = render(shellBootstrapper.rootElement);
+        let appShell = new AppShell();
+        appShell.start();
+        const renderResult = render(
+            <AppShellView router={appShell.router} workspaceModelId={appShell.workspaceModelId} />
+        );
         let appRoot = renderResult.getByRole('app-root');
         expect(appRoot).toBeInTheDocument();
     });

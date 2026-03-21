a6e28091ae510cd0748eec4653cc7f1ae83b77b4
Keith Woods <devshop7@gmail.com>
2026-03-21 13:02:37 +0800

    region item update

--- packages/esp-js-ui/src/ui/regions/views/regionItemRecordView.tsx
+++ packages/esp-js-ui/src/ui/regions/views/regionItemRecordView.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {RegionItemRecord} from '../models';
 import {ConnectableComponent} from 'esp-js-react';
 
-export interface RegionItemRecordViewProps  {
+export interface RegionItemRecordViewProps {
     regionItemRecord: RegionItemRecord;
     showLoadingUi?: boolean;
     loadingMessage?: string;
@@ -10,24 +10,21 @@ export interface RegionItemRecordViewProps  {
     style?: any;
 }
 
-export class RegionItemRecordView extends React.Component<RegionItemRecordViewProps, any> {
-    render() {
-        const {regionItemRecord, showLoadingUi, loadingMessage, className, style} = this.props;
-        let loadingComponent = null;
-        if (showLoadingUi) {
-            if (regionItemRecord.hasError) {
-                loadingComponent = (<div>{loadingMessage ? loadingMessage : 'Load Error'}</div>);
-            } else {
-                loadingComponent = (<div>{loadingMessage ? loadingMessage : 'Waiting For View To Load'}</div>);
-            }
+export const RegionItemRecordView = ({regionItemRecord, showLoadingUi, loadingMessage, className, style}: RegionItemRecordViewProps) => {
+    let loadingComponent = null;
+    if (showLoadingUi) {
+        if (regionItemRecord.hasError) {
+            loadingComponent = (<div>{loadingMessage ? loadingMessage : 'Load Error'}</div>);
+        } else {
+            loadingComponent = (<div>{loadingMessage ? loadingMessage : 'Waiting For View To Load'}</div>);
         }
-        return (
-            <div style={style} className={className || 'item-item-container'}>
-                {regionItemRecord.modelCreated
-                    ? <ConnectableComponent modelId={regionItemRecord.modelId} viewContext={regionItemRecord.displayContext}/>
-                    : loadingComponent
-                }
-            </div>
-        );
     }
-}
+    return (
+        <div style={style} className={className || 'item-item-container'}>
+            {regionItemRecord.modelCreated
+                ? <ConnectableComponent modelId={regionItemRecord.modelId} viewContext={regionItemRecord.displayContext}/>
+                : loadingComponent
+            }
+        </div>
+    );
+};
